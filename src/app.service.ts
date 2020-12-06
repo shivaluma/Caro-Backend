f5605dda925caf4a8750c3bebf85bb5e9580a36b
shivaluma <shivaluma@gmail.com>
2020-12-06 19:28:29 +0700

    Add Swagger

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World from asd23!';
+  }
+
+  postHello(): string {
+    return 'Post re!';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = new DocumentBuilder()
+    .setTitle('Caro API')
+    .setDescription('The Caro API description')
+    .setVersion('1.0')
+    .addTag('caro')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(4000);
 }
 bootstrap();
