fc74a9dea619b7e73caacbe12c283cb5a36b9165
crepp-tt <tan.thai.2t@gmail.com>
2020-12-11 11:15:54 +0700

    Add admin login

---
+++ src/controllers/admin/auth.controller.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const got = require('got');
+const { ResponseService, UserService } = require('../services');
+
+exports.postSignInAdmin = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json(ResponseService.error(400, 'No email/password was provided.'));
+  }
+
+  try {
+    const user = await UserService.findOne({ email });
+    req.log.info(user);
+    if (!user)
+      return res
+        .status(404)
+        .json(
+          ResponseService.error(
+            404,
+            'Cannot find account with this email.',
+            null,
+          ),
+        );
+
+    const isSamePassword = await argon2.verify(user.password, password);
+
+    if (!isSamePassword) {
+      return res
+        .status(400)
+        .json(ResponseService.error(400, 'Wrong email or password.', null));
+    }
+
+    if (user.role != 'Admin') {
+      return res
+        .status(403)
+        .json(ResponseService.error(403, 'Does not have permission.'));
+    }
+
+    const payload = {
+      id: user._id,
+      email: user.email,
+      displayName: user.displayName,
+      role: user.role,
+    };
+    const accessToken = jwt.sign(payload, process.env.SECRET_KEY);
+    return res.status(200).json(
+      ResponseService.response(200, 'Login Successfully.', {
+        accessToken,
+        user: payload,
+      }),
+    );
+  } catch (err) {
+    return res
+      .status(500)
+      .json(ResponseService.error(500, 'Server error.', err));
+  }
+};
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const AuthController = require('../controllers/auth.controller');
+const AdminAuthController = require('../controllers/admin/auth.controller');
 
 router.post('/signin', AuthController.postSignIn);
 router.post('/signup', AuthController.postSignUp);
@@ -7,5 +8,6 @@ router.post('/signin-google', AuthController.postGoogleSignIn);
 router.post('/signin-facebook', AuthController.postFacebookSignin);
 router.get('/check-field', AuthController.getValidField);
 
-router.post('/admin/signin', AuthController.postSignIn);
+//admin route
+router.post('/admin/signin', AdminAuthController.postSignInAdmin);
 module.exports = router;
