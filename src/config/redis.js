27ec73b5278205ca26bab7821022d943aeb83b40
crepp-tt <tan.thai.2t@gmail.com>
2021-01-13 14:55:23 +0700

    Fix quick match

--- src/config/redis.js
+++ src/config/redis.js
@@ -4,7 +4,7 @@ const redis = require('redis');
 bluebird.promisifyAll(redis);
 
 const client = redis.createClient({
-  host: process.env.REDIS_HOST || '10.126.4.69',
+  host: process.env.REDIS_HOST || 'localhost',
   port: process.env.REDIS_PORT || 6379,
   password: process.env.REDIS_PASS || 'pppp',
 });
