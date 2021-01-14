658597237d43207b56720a3d69c079a79bd9af5f
shivaluma <shivaluma@gmail.com>
2021-01-14 15:28:45 +0700

    Disable f5 lost data

--- src/socket/online.js
+++ src/socket/online.js
@@ -29,63 +29,63 @@ module.exports = (socket, io) => {
     socket.user = null;
     await redis.delAsync(`users:${user._id}`);
 
-    let roomId = null;
+    // let roomId = null;
 
-    if (socket.room && Number.isInteger(socket.room.roomId)) {
-      roomId = socket.room.roomId;
-    }
+    // if (socket.room && Number.isInteger(socket.room.roomId)) {
+    //   roomId = socket.room.roomId;
+    // }
 
-    if (Number.isInteger(roomId)) {
-      socket.to(`room-${roomId}`).emit('user-leave-room', user);
-      let { room } = socket;
-      if (!room) room = roomService.rooms[roomId];
-      if (!room) return;
+    // if (Number.isInteger(roomId)) {
+    //   socket.to(`room-${roomId}`).emit('user-leave-room', user);
+    //   let { room } = socket;
+    //   if (!room) room = roomService.rooms[roomId];
+    //   if (!room) return;
 
-      const index = room.people.findIndex((el) => el._id === user._id);
+    //   const index = room.people.findIndex((el) => el._id === user._id);
 
-      if (index !== -1) room.people.splice(index, 1);
-      if (room.people.length === 0) {
-        setTimeout(() => {
-          roomService.rooms[roomId] = null;
-          room = null;
-          io.emit('clear-room', roomId);
-        }, 0);
-      }
+    //   if (index !== -1) room.people.splice(index, 1);
+    //   if (room.people.length === 0) {
+    //     setTimeout(() => {
+    //       roomService.rooms[roomId] = null;
+    //       room = null;
+    //       io.emit('clear-room', roomId);
+    //     }, 0);
+    //   }
 
-      if (room.people.length > 0) {
-        if (room.owner._id === user._id) {
-          [room.owner] = room.people;
-          socket.to(`room-${roomId}`).emit('room-owner-change', room.people[0]);
-        }
+    //   if (room.people.length > 0) {
+    //     if (room.owner._id === user._id) {
+    //       [room.owner] = room.people;
+    //       socket.to(`room-${roomId}`).emit('room-owner-change', room.people[0]);
+    //     }
 
-        if (room.firstPlayer && room.firstPlayer._id === user._id) {
-          socket.to(`room-${roomId}`).emit('player-change-side', {
-            user,
+    //     if (room.firstPlayer && room.firstPlayer._id === user._id) {
+    //       socket.to(`room-${roomId}`).emit('player-change-side', {
+    //         user,
 
-            side: null,
-            leaveSide: 1,
-            userTurn: null,
-          });
+    //         side: null,
+    //         leaveSide: 1,
+    //         userTurn: null,
+    //       });
 
-          room.firstPlayer = null;
-        }
+    //       room.firstPlayer = null;
+    //     }
 
-        if (room.secondPlayer && room.secondPlayer._id === user._id) {
-          socket.to(`room-${roomId}`).emit('player-change-side', {
-            user,
-            roomId,
-            side: null,
-            leaveSide: 2,
-            userTurn: null,
-          });
+    //     if (room.secondPlayer && room.secondPlayer._id === user._id) {
+    //       socket.to(`room-${roomId}`).emit('player-change-side', {
+    //         user,
+    //         roomId,
+    //         side: null,
+    //         leaveSide: 2,
+    //         userTurn: null,
+    //       });
 
-          room.secondPlayer = null;
-        }
-      }
+    //       room.secondPlayer = null;
+    //     }
+    //   }
 
-      socket.room = null;
-      socket.leave(`room-${roomId}`);
-    }
+    //   socket.room = null;
+    //   socket.leave(`room-${roomId}`);
+    // }
 
     removeUser(user);
   });
